c1a4c934d8c3d65b5a3ce78d81147941e30dc7a3
Parmeet Singh <sainijoy.parmeet54@gmail.com>
2022-09-15 17:04:09 -0700

    Add Express Backend

---
+++ 295a-node-backend/index.js
@@ -0,0 +1,21 @@
+// import dependencies
+const express = require('express');
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// import routes here
+
+// Backend runs on Port 3000
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Backend is running at port:", port);
+  });
