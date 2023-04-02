cb8cc5db8060db4362da5ab2062d6f7ba4c48928
Ryan Tran <ryantran2165@gmail.com>
2023-04-01 19:40:00 -0700

    Continue classification model

--- cmpe295a-ml/main.py
+++ cmpe295a-ml/main.py
@@ -1,5 +1,51 @@
 import cv2
+import torch
+from torch import nn
 
+n_classes = 10
+
+class_names = [str(num) for num in range(10)] + \
+              [chr(capital) for capital in range(ord('A'), ord('Z') + 1)] + \
+              [chr(lower) for lower in range(ord('a'), ord('z') + 1)]
+
+class CNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=32),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=5, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=32),
+            nn.Dropout(p=0.4),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=64),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=5, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.BatchNorm2d(num_features=64),
+        )
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=0.4),
+            nn.Linear(in_features=64*20*20, out_features=128),
+            nn.ReLU(),
+            nn.BatchNorm1d(num_features=128),
+            nn.Dropout(p=0.4),
+            nn.Linear(in_features=128, out_features=n_classes),
+        )
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
 
 def intersects(a, b):
     x1, y1, x2, y2 = a
@@ -45,7 +91,7 @@ def filter_rects(rects, min_area, max_area):
 
 
 def main():
-    name = 'smallest_even_multiple'
+    name = 'digits'
 
     img = cv2.imread(f'{name}.jpg')
 
@@ -92,6 +138,28 @@ def main():
     for x1, y1, x2, y2 in rects:
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
     cv2.imwrite(f'{name}_boxes.jpg', img)
+
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+    print(f"Using {device} device")
+    model = CNN()
+    model.load_state_dict(torch.load("model_digits.pth"))
+    model = model.to(device)
+    model.eval()
+    with torch.no_grad():
+        for i, (x1, y1, x2, y2) in enumerate(rects):
+            roi = gray[y1:y2, x1:x2]
+            size = 20
+            roi = cv2.resize(roi, (size, size))
+            roi = cv2.bitwise_not(roi)
+            # roi[roi < 128] = 0
+            # roi[roi >= 128] = 255
+            border = (28 - size) // 2
+            roi = cv2.copyMakeBorder(roi, border, border, border, border, cv2.BORDER_CONSTANT, value=(0,0,0))
+            tensor = torch.from_numpy(roi.astype('float32')).unsqueeze(0).unsqueeze(0).to(device)
+            pred = model(tensor)
+            pred = class_names[pred[0].argmax(0)]
+            print('Predicted:', pred)
+            cv2.imwrite(f'preds/{i}_{pred}.jpg', roi)
 
 
 if __name__ == '__main__':
