2926694b5299dc91129448b799d5a765babdc3a7
Ryan Tran <ryantran2165@gmail.com>
2023-04-14 14:29:06 -0700

    Train balanced classifier with basic math symbols and colon, improve main preprocessing

--- cmpe295a-ml/main.py
+++ cmpe295a-ml/main.py
@@ -3,14 +3,35 @@ import numpy as np
 import torch
 from torch import nn
 
+classifier_name = "emnistbalanced_mathsymbols_custom"
+
 # math_symbols = ["-", "(", ")", ",", "[", "]", "+", "=", "forward_slash", "gt", "lt", "times"]
-math_symbols = ["-", "+", "=", "forward_slash", "gt", "lt", "times"]
-class_names = (
-    [str(num) for num in range(10)]
-    + [chr(capital) for capital in range(ord("A"), ord("Z") + 1)]
-    + [chr(lower) for lower in range(ord("a"), ord("z") + 1)]
-    + math_symbols
-)
+math_symbols = ["gt"]
+
+custom_symbols = ["colon"]
+
+class_names = []
+
+if "mnist" in classifier_name:
+    class_names += [str(num) for num in range(10)]
+
+if "emnist" in classifier_name:
+    class_names += [chr(capital) for capital in range(ord("A"), ord("Z") + 1)]
+    class_names += [chr(lower) for lower in range(ord("a"), ord("z") + 1)]
+
+if "balanced" in classifier_name:
+    class_names = [
+        x
+        for x in class_names
+        if x not in {"c", "i", "j", "k", "l", "m", "o", "p", "s", "u", "v", "w", "x", "y", "z"}
+    ]
+
+if "symbols" in classifier_name:
+    class_names += math_symbols
+
+if "custom" in classifier_name:
+    class_names += custom_symbols
+
 n_classes = len(class_names)
 
 
@@ -54,7 +75,7 @@ class CNN(nn.Module):
         return out
 
 
-def intersects(a, b):
+def intersects(a, b, min_area_percent=0):
     x1, y1, x2, y2 = a
     x3, y3, x4, y4 = b
     x_overlap = max(0, min(x2, x4) - max(x1, x3))
@@ -62,7 +83,7 @@ def intersects(a, b):
     overlap_area = x_overlap * y_overlap
     area1 = (x2 - x1) * (y2 - y1)
     area2 = (x4 - x3) * (y4 - y3)
-    return overlap_area > 0.9 * min(area1, area2)
+    return overlap_area > min_area_percent * min(area1, area2)
 
 
 def merge(a, b):
@@ -81,7 +102,36 @@ def merge_all(rects):
         while i < len(rects) - 1:
             j = i + 1
             while j < len(rects):
-                if intersects(rects[i], rects[j]):
+                if intersects(rects[i], rects[j], 0.9):
+                    hasMerge = True
+                    rects[i] = merge(rects[i], rects[j])
+                    del rects[j]
+                else:
+                    j += 1
+            i += 1
+
+
+def merge_noncontinuous(rects):
+    avg_width = sum(x2 - x1 for (x1, y1, x2, y2) in rects) / len(rects)
+    avg_height = sum(y2 - y1 for (x1, y1, x2, y2) in rects) / len(rects)
+    avg_dst = np.sqrt(avg_width**2 + avg_height**2)
+    print("avg dims:", avg_width, avg_height)
+
+    hasMerge = True
+    while hasMerge:
+        hasMerge = False
+        i = 0
+        while i < len(rects) - 1:
+            x1, y1, x2, y2 = rects[i]
+            cx1 = (x1 + x2) / 2
+            cy1 = (y1 + y2) / 2
+            j = i + 1
+            while j < len(rects):
+                x3, y3, x4, y4 = rects[j]
+                cx2 = (x3 + x4) / 2
+                cy2 = (y3 + y4) / 2
+                dst = np.sqrt((cx1 - cx2) ** 2 + (cy1 - cy2) ** 2)
+                if ((y2 < y3) or (y4 < y1)) and dst < 1.0 * avg_dst:
                     hasMerge = True
                     rects[i] = merge(rects[i], rects[j])
                     del rects[j]
@@ -103,7 +153,9 @@ def filter_rects(rects, min_area, max_area):
 
 
 def main():
+    # name = "sharpie"
     name = "simple_if"
+    # name = "smallest_even_multiple_gray"
 
     img = cv2.imread(f"{name}.jpg")
 
@@ -141,26 +193,28 @@ def main():
     min_area = max_a * 0.005
     max_area = 100000
 
-    # Merge intersecting rectangles, filter, and sort by horizontal position
+    # Merge and clean intersecting rectangles, filter by area, and sort by horizontal position
     merge_all(rects)
     rects = filter_rects(rects, min_area, max_area)
     rects.sort(key=lambda rect: rect[0])
+
+    # Non-continuous characters: i, j, :, and =
+    merge_noncontinuous(rects)
+
+    # Periods and commas: . and ,
+
+    # Quotation marks: ' and "
 
     # Draw bounding boxes
     for x1, y1, x2, y2 in rects:
         cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
     cv2.imwrite(f"{name}_boxes.jpg", img)
 
-    device = (
-        "cuda"
-        if torch.cuda.is_available()
-        else "mps"
-        if torch.backends.mps.is_available()
-        else "cpu"
-    )
+    # Inference
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Using {device} device")
     model = CNN()
-    model.load_state_dict(torch.load("model_emnistbyclass_mathsymbols.pth"))
+    model.load_state_dict(torch.load(f"model_{classifier_name}.pth"))
     model = model.to(device)
     model.eval()
     with torch.no_grad():
@@ -168,8 +222,17 @@ def main():
             # Get region of interest from grayscale image
             roi = gray[y1:y2, x1:x2]
 
-            # Resize, maintain aspect ratio, shrink larger dimension to 20 pixels
-            resize_size = 20
+            # Flip black and white
+            flipped = cv2.bitwise_not(roi)
+
+            # Ensure black background
+            flipped[flipped < 85] = 0
+
+            # Gaussian blur
+            blurred = cv2.GaussianBlur(flipped, ksize=(3, 3), sigmaX=1, sigmaY=1)
+
+            # Resize, maintain aspect ratio, shrink larger dimension to 24 pixels
+            resize_size = 24
             width = x2 - x1
             height = y2 - y1
             if width > height:
@@ -178,39 +241,33 @@ def main():
             else:
                 new_height = resize_size
                 new_width = int(new_height * (width / height))
-            resized = cv2.resize(roi, (new_width, new_height))
+            resized = cv2.resize(blurred, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
 
-            # Flip black and white
-            flipped = cv2.bitwise_not(resized)
-
-            # Ensure black background
-            flipped[flipped < 85] = 0
-
-            # Add black padding border
+            # Add 2 pixel black padding
             pad_size = 28
             padded = np.zeros((pad_size, pad_size))
             x_start = pad_size // 2 - new_width // 2
             x_end = x_start + new_width
             y_start = pad_size // 2 - new_height // 2
             y_end = y_start + new_height
-            padded[y_start:y_end, x_start:x_end] = flipped
+            padded[y_start:y_end, x_start:x_end] = resized
 
-            # Noramlize
+            # Normalize
             normalized = padded / 255
-
-            # Keep only the largest continuous stroke, but that messes with letters like 'i'
 
             # Convert to tensor
             tensor = (
                 torch.from_numpy(normalized.astype("float32")).unsqueeze(0).unsqueeze(0).to(device)
             )
 
-            # Predit
+            # Predict
             pred = model(tensor)
             pred = class_names[pred[0].argmax(0)]
             print("Predicted:", pred)
 
-            cv2.imwrite(f"preds/roi_{i}_{pred}.jpg", padded)
+            cv2.imwrite(f"preds/{i}_{pred}.jpg", padded)
+
+    # Split into lines and tokens
 
 
 if __name__ == "__main__":
